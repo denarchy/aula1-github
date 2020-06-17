8b88bd4f545aef1b6737facf1b0fe031a740d512
Victor Tommaz <victortommaz@gmail.com>
2020-06-17 20:53:29 -0300

    Upgrade do programa MAIN eu te amo nenein

--- src/Application/GitProgram.java
+++ src/Application/GitProgram.java
@@ -1,10 +1,24 @@
 package Application;
 
+import java.util.Scanner;
+
 public class GitProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);	
+		
+		boolean andressa = true;
+		String palavra;
+		System.out.println("Voce me ama me responde nene: ");
+		palavra = sc.nextLine();
+		while(palavra.equals("sim")) {
+			System.out.println("Então eu te amo");
+			palavra = sc.nextLine();
+			
+		}
+		
+		System.out.println("Entao vai dormir mocreia");
 	}
 
 }
